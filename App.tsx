547721ad1b22f75a789d745729b233bd8c9a4ebc
Anthony E <e4nthony@gmail.com>
2023-03-28 17:25:37 +0300

    ui: created student image and txt field

--- App.tsx
+++ App.tsx
@@ -1,38 +1,75 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+// import { NavigationContainer } from '@react-navigation/native';
+import {
+  StyleSheet, Text, TextInput, View, Image
+} from 'react-native';
 
 const App = () => {
   console.log("App started.");
+
+  const [id, setId] = useState("");
 
   const onPressCallback = () => {
     console.log("Button is pressed.");
   }
 
   return (
-    <View style={styles.container}>
-      <View style={styles.box1}></View>
+    <View style={styles.containerMain}>
+      <Image style={styles.avatarImageStyle}
+        source={require('./assets/man.png')}></Image>
+
+      <TextInput
+        style={styles.inputField}
+        onChangeText={setId}
+        value={id}
+        placeholder={'Student ID'}
+      >
+      </TextInput>
+      {/* <TextInput
+        style={styles.inputField}
+        
+      >
+      </TextInput> */}
+
+      {/* <View style={styles.box1}></View>
       <View style={styles.box2}></View>
-      <View style={styles.box3}></View>
+      <View style={styles.box3}></View> */}
     </View >
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  containerMain: {
+    // marginTop: 20 ,
     flex: 1,
-    backgroundColor: 'grey',
+    backgroundColor: 'lightgrey',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  box1: {
-    height: 100,
+  // container: {
+  //   flex: 1,
+  //   backgroundColor: 'lightgrey',
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  // },
+  avatarImageStyle: {
+    margin: 10 ,
+    height: 200,
+    width: 200,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputField: {
+    // height: 100,
     width: 100,
-    flex: 1,
-    backgroundColor: '#001',
+    backgroundColor: 'green',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+
+
+
   box2: {
     height: 100,
     width: 100,
